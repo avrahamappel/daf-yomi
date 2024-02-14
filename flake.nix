e718faefff0025fe57f0034613b4524e21759fd6
Avraham Appel <aappel248@gmail.com>
2024-02-13 21:27:04 -0500

    elm wrapper

--- flake.nix
+++ flake.nix
@@ -6,13 +6,24 @@
       let
         pkgs = import nixpkgs { inherit system; };
 
+        elmWrapper = pkgs.writeShellScriptBin "elm" ''
+          ${pkgs.elmPackages.elm}/bin/elm "$@"
+
+          # Update manifests if elm.json changed
+          if [[ "$(git diff --name-only)" =~ elm.json ]]; then
+            echo 'elm.json changed, updating manifest files'
+            elm2nix convert > elm-srcs.nix
+            elm2nix snapshot
+          fi
+        '';
+
         packageJson = builtins.fromJSON (builtins.readFile ./package.json);
         elmJson = builtins.fromJSON (builtins.readFile ./elm.json);
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; with elmPackages; [
-            elm
+            elmWrapper
             elm-language-server
             elm-format
             elm2nix
