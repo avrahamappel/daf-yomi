da7e6431a3f278bfeff6b51dbb0817a229e236de
Avraham Appel <aappel248@gmail.com>
2024-02-20 23:00:47 -0500

    Install correctly
    The binary makes assumptions about dependencies in its directory, so a
    wrapper is necessary

--- simulator.nix
+++ simulator.nix
@@ -1,26 +1,31 @@
 { pkgs ? import <nixpkgs> { }, stdenv ? pkgs.stdenv }:
 
-stdenv.mkDerivation {
-  name = "kaios-simulator";
-  src = pkgs.fetchzip {
-    url = "https://s3.amazonaws.com/kaicloudsimulatordl/developer-portal/simulator/Kaiosrt_ubuntu.tar.bz2";
-    hash = "sha256-BAe5hnAa/mBDznmt4Wy6tVJZcGw0Vx09srtv2o9p0h0=";
+let
+  kaiosrt = stdenv.mkDerivation {
+    name = "kaiosrt";
+    src = pkgs.fetchzip {
+      url = "https://s3.amazonaws.com/kaicloudsimulatordl/developer-portal/simulator/Kaiosrt_ubuntu.tar.bz2";
+      hash = "sha256-BAe5hnAa/mBDznmt4Wy6tVJZcGw0Vx09srtv2o9p0h0=";
+    };
+
+    buildInputs = [ pkgs.autoPatchelfHook ];
+
+    nativebuildInputs = with pkgs; [
+      pkg-config
+      glib
+    ];
+
+    autoPatchelfIgnoreMissingDeps = [ "*" ];
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/bin
+      tar -xvf $src/kaiosrt-v2.5.en-US.linux-x86_64.tar.bz2 --directory $out/bin
+      runHook postInstall
+    '';
   };
+in
 
-  buildInputs = [ pkgs.autoPatchelfHook ];
-
-  nativebuildInputs = with pkgs; [
-    pkg-config
-    glib
-  ];
-
-  autoPatchelfIgnoreMissingDeps = [ "*" ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin $out/lib
-    tar -xvf $src/kaiosrt-v2.5.en-US.linux-x86_64.tar.bz2 --directory $out/lib
-    install -m755 -D $out/lib/kaiosrt/kaiosrt $out/bin/kaiosrt
-    runHook postInstall
-  '';
-}
+pkgs.writeShellScriptBin "kaios-simulator" ''
+  ${kaiosrt}/bin/kaiosrt/kaiosrt "$@"
+''
