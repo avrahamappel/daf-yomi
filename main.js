67d56697eab8290651ed78f578b3290aeea08693
Avraham Appel <aappel248@gmail.com>
2024-09-23 22:09:36 -0400

    Introduce settings model

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ import './style.css'
 import { Elm } from './src/Main.elm'
 import { getData } from './src/data'
 import { getLocation } from './src/location'
+import { getSettings } from './src/settings';
 
 const app = Elm.Main.init({ node: document.getElementById('app') });
+const initialSettings = getSettings();
 
-getLocation((pos) => app.ports.setLocation.send(pos));
+getLocation(initialSettings, (pos) => app.ports.setLocation.send(pos));
 
 app.ports.getData.subscribe(({ timestamp, position }) => {
     const data = getData(timestamp, position);
