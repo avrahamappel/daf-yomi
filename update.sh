f96e4d9c47ae2de060cc61ee23a1a6769b61268e
Avraham Appel <aappel248@gmail.com>
2024-09-29 12:39:10 -0400

    Update update script and action

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
 # Update nix flake
 nix flake update
 
@@ -9,3 +11,11 @@ npm update
 # Rebuild elm nix manifests
 elm2nix convert > elm-srcs.nix
 elm2nix snapshot
+
+# Update NPM deps hash if necessary
+if ! output="$(nix build --no-link |& tee /dev/tty)"; then
+  new_hash="$(echo "$output" | awk '/got:/ {print $2}')"
+
+  echo "Updating NPM dependencies hash to [$new_hash]"
+  sed -i "s/npmDepsHash = \".*\"/npmDepsHash = \"$new_hash\"/" flake.nix
+fi
