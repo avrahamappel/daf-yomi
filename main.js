156de9c22f4071c26c9cc183381cf83f47cf95dc
Avraham Appel <aappel248@gmail.com>
2024-10-06 18:34:46 -0400

    Add getLocation port

--- main.js
+++ main.js
@@ -5,11 +5,20 @@ import { getLocation } from './src/location'
 import { getSettings } from './src/settings';
 
 const app = Elm.Main.init({ node: document.getElementById('app') });
-const initialSettings = getSettings();
 
-getLocation(initialSettings, (pos) => app.ports.setLocation.send(pos), (error) => app.ports.receiveError.send(error));
+const sendLocationToElm = (settings) => getLocation(
+    settings,
+    (pos) => app.ports.setLocation.send(pos),
+    (error) => app.ports.receiveError.send(error)
+);
 
 app.ports.getData.subscribe(({ settings, timestamp, position }) => {
     const data = getData(settings, timestamp, position);
     app.ports.returnData.send(data);
 })
+
+// If location method changes, app will re-request location
+app.ports.getLocation.subscribe(settings => sendLocationToElm(settings));
+
+const initialSettings = getSettings();
+sendLocationToElm(initialSettings);
