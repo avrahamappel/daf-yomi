2d20253bfda3da59cb9a9419caded3d7619bf85e
Avraham Appel <33736292+avrahamappel@users.noreply.github.com>
2024-10-06 22:18:29 -0400

    Fix sed expression

--- update.sh
+++ update.sh
@@ -17,5 +17,5 @@ if ! output="$(nix build --no-link |& tee /dev/tty)"; then
   new_hash="$(echo "$output" | awk '/got:/ {print $2}')"
 
   echo "Updating NPM dependencies hash to [$new_hash]"
-  sed -i "s/npmDepsHash = \".*\"/npmDepsHash = \"$new_hash\"/" flake.nix
+  sed -i "s#npmDepsHash = \".*\"#npmDepsHash = \"$new_hash\"#" flake.nix
 fi
