8fcf276c85676cbb20d6f0fc160e5d454588bde3
Avraham Appel <aappel248@gmail.com>
2025-09-29 08:46:19 -0400

    init

---
+++ src/location.rs
@@ -0,0 +1,9 @@
+struct Position {
+    name: Option<String>,
+    longitude: f64,
+    latitude: f64,
+    altitude: Option<f64>,
+}
+
+pub fn get_location(settings: Settings, next: fn(Position) -> (), error: fn(String) -> ()) {
+}
