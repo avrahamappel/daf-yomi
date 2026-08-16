e9544b173e3240fb6046bf2370b5199e59c7a387
Avraham Appel <aappel248@gmail.com>
2026-08-16 14:17:00 -0400

    Format, flesh out test a bit

--- tests/MainPageTest.elm
+++ tests/MainPageTest.elm
@@ -1,29 +1,46 @@
 module MainPageTest exposing (..)
 
 import Expect
-import Main exposing (Model)
+import Main exposing (Msg(..), update)
 import Test exposing (..)
+import Time exposing (millisToPosix)
+
 
 tests : Test
-tests = describe "Main page" [ describe "Date switching bug" [
-    test "it flips to the next date when right date button is pressed" <|
-        \_ ->
-            let
-                initModel = {}
-            in
+tests =
+    describe "Main page"
+        [ describe "Date switching bug"
+            [ test "it flips to the next date when right date button is pressed" <|
+                \_ ->
+                    let
+                        initModel =
+                            { date = "today", time = "now" }
 
-            Expect.equal initModel initModel
-    ] ]
+                        nextDateModel =
+                            update (UpdateTime (millisToPosix 1000)) initModel
+
+                        tick =
+                            update nextDateModel
+                    in
+                    Expect.all
+                        [ Expect.equal initModel initModel
+                        , Expect.equal nextDateModel { date = "tomorrow" }
+                        , Expect.equal tick { date = "tomorrow", time = "+1 second" }
+                        ]
+            ]
+        ]
+
+
 
 {-
-current date = tues aug 11 9:50pm
-timezone = toronto
-location = (43.71, -79.40)
-displayed zeman = 9 (chatzos halayla)
+   current date = tues aug 11 9:50pm
+   timezone = toronto
+   location = (43.71, -79.40)
+   displayed zeman = 9 (chatzos halayla)
 
-act:
-flip to next date
-assert next date
-update current time by one second
-assert date has not changed
+   act:
+   flip to next date
+   assert next date
+   update current time by one second
+   assert date has not changed
 -}
